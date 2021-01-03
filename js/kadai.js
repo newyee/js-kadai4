92f009ad060057ea45fb7b2f9277a00b9438bd22
newyee <takatelin01@icloud.com>
2021-01-03 21:07:12 +0900

    getQuizDataをawaitで待つように実装

---
+++ js/kadai.js
@@ -0,0 +1,41 @@
+'use strict'
+
+window.addEventListener('DOMContentLoaded', () => {
+  const startButton = document.getElementById('start_button')
+  const titleText = document.getElementById('title')
+  const descriptionText = document.getElementById('description')
+  startButton.addEventListener('click', async () => {
+    startButton.remove()
+    titleText.textContent = '取得中'
+    descriptionText.textContent = '少々お待ち下さい'
+    console.log('a')
+    const responsData = await getQuizData()
+    console.log(responsData)
+    console.log('b')
+
+
+    // console.log(await getData())
+    // getData().then((value) => {
+    //   console.log('aaa')
+    // })
+    // console.log('b')
+
+  })
+  const getQuizData = async () => {
+    const respons = await fetch('https://opentdb.com/api.php?amount=10')
+    const data = await respons.json();
+    // console.log(data.results)
+    // console.log(data)
+    return data
+  }
+  // const getData = async () => {
+  //   return new Promise(async (resolve, _reject) => {
+  //     const respons = await fetch('https://opentdb.com/api.php?amount=10')
+  //     const data = await respons.json();
+  //     resolve(data);
+  //   })
+  // }
+
+})
+
+
