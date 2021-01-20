b82898f10f92067e0eafc4748b546c2c04b6eec1
newyee <takatelin01@icloud.com>
2021-01-20 18:58:59 +0900

    インスタンスの作成

--- js/kadai.js
+++ js/kadai.js
@@ -1,20 +1,21 @@
 'use strict'
 class Quiz{
   constructor(genre,difficulty,question,correctAnswer,incorrect_answers){
-    this.genre = genre
-    this.difficulty = difficulty
-    this.question = question
-    this.choiceList = incorrect_answers
-    this.choiceList.push(correctAnswer)
+    this.quiz_genre = genre
+    this.quiz_difficulty = difficulty
+    this.quiz_question = question
+    this.quiz_choiceList = incorrect_answers
+    this.quiz_choiceList.push(correctAnswer)
+    this.quiz_choiceList = this.shuffle(this.quiz_choiceList)
   }
   get genre(){
-    return this.genre
+    return this.quiz_genre
   }
   get difficulty(){
-    return this.genre
+    return this.quiz_difficulty
   }
   get question(){
-    return this.genre
+    return this.quiz_question
   }
   shuffle ([...array]) {
     for (let i = array.length - 1; i >= 0; i--) {
@@ -43,9 +44,30 @@ window.addEventListener('DOMContentLoaded', () => {
     descriptionText.textContent = '少々お待ち下さい'
     responsData = await getQuizData()
     results = responsData.results
+    console.log('results',results)
     resultLength = results.length
-
+    let quiz_one = new Quiz(results[0].category,results[0].difficulty,results[0].question,results[0].correctAnswer,
+      results[0].incorrect_answers);
+    let quiz_two = new Quiz(results[1].category,results[1].difficulty,results[1].question,results[1].correctAnswer,
+      results[1].incorrect_answers);
+    let quiz_three = new Quiz(results[2].category,results[2].difficulty,results[2].question,results[2].correctAnswer,
+      results[2].incorrect_answers);
+    let quiz_four = new Quiz(results[3].category,results[3].difficulty,results[3].question,results[3].correctAnswer,
+      results[3].incorrect_answers);
+    let quiz_five = new Quiz(results[4].category,results[4].difficulty,results[4].question,results[4].correctAnswer,
+      results[4].incorrect_answers);
+    let quiz_six = new Quiz(results[5].category,results[5].difficulty,results[5].question,results[5].correctAnswer,
+      results[5].incorrect_answers);
+    let quiz_seven = new Quiz(results[6].category,results[6].difficulty,results[6].question,results[6].correctAnswer,
+      results[6].incorrect_answers);
+    let quiz_eight = new Quiz(results[7].category,results[7].difficulty,results[7].question,results[7].correctAnswer,
+      results[7].incorrect_answers);
+    let quiz_nine = new Quiz(results[8].category,results[8].difficulty,results[8].question,results[8].correctAnswer,
+      results[8].incorrect_answers);
+    let quiz_ten = new Quiz(results[9].category,results[9].difficulty,results[9].question,results[9].correctAnswer,
+      results[9].incorrect_answers);
     startQuiz()
+
   })
   const startQuiz = async () => {
     // let oneQuestion = new Quiz(results[0].category,)
